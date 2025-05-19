88441bb5c46dedd41bafb9fa5a8852197ca5d6dd
sbg224 <sambah450@gmail.com>
2025-05-19 22:48:12 +0200

    bug fixe

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,25 @@ const nextConfig = {
 	env: {
 		NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
 	},
+	headers: async () => [
+		{
+			source: "/(.*)", // Toutes les pages
+			headers: [
+				{
+					key: "Cache-Control",
+					value: "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0",
+				},
+				{
+					key: "Pragma",
+					value: "no-cache",
+				},
+				{
+					key: "Expires",
+					value: "0",
+				},
+			],
+		},
+	],
 };
 
 module.exports = nextConfig;
